6c3d95a57b5241c1b6c510f246b1a42ae21f73ac
Jungwoong Kim <dk040106@naver.com>
2020-05-15 15:04:41 +0900

    fix: font problems

--- src/components/Goals.svelte
+++ src/components/Goals.svelte
@@ -1,12 +1,10 @@
 <div class="goals">
   <h1>Goals</h1>
-  <h2>Goal of this month(May 2020)</h2>
-  <ol>
-    <li>
-      Start a YouTube channel and upload 3 videos about making gadgets with
-      basic materials. (with arduino and raspberry pi)
-    </li>
-  </ol>
+  <h2>Goal of this month (May 2020)</h2>
+  <p>
+    1. Start a YouTube channel and upload 3 videos about making gadgets with
+    basic materials. (with arduino and raspberry pi)
+  </p>
 
   <h2>My past Goals</h2>
   <p>Not much things here yet!</p>
